99aa56a3d4084baa13c6bcaaf37d9da2c950daad
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-01 22:44:08 +0200

    Change todo list item data and class name according props

--- client/src/components/TodoListItem.jsx
+++ client/src/components/TodoListItem.jsx
@@ -1,8 +1,10 @@
 export default function TodoListItem({ todo }) {
+    const changeClassName = todo.isCompleted ? "todo is-completed" : "todo";
+
     return (
-        <tr className="todo">
-            <td>Vacuum floor</td>
-            <td>Incomplete</td>
+        <tr className={changeClassName}>
+            <td>{todo.text}</td>
+            <td>{todo.isCompleted ? "Complete" : "Incomplete"}</td>
             <td className="todo-action">
                 <button className="btn todo-btn">Change status</button>
             </td>
